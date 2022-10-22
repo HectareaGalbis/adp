136a34ca6015738a6a06d0204ce4278101579e0e
Hectarea1996 <hectometrocuadrado@gmail.com>
2022-10-22 09:56:01 +0200

    Update

--- adp.lisp
+++ adp.lisp
@@ -76,40 +76,46 @@
 
 (adv-subheader "Literate programming functions" literate-subheader)
 
-(adv-defmacro header (str &optional label)
+(adv-defmacro header (str &optional tag)
+  "Add a header with name str. Also, if tag is not nil but a symbol, a new header-tag is created."	      
   (when adppvt:*add-documentation*
     (check-type str string "a string")
-    (check-type label (or null symbol) "a symbol")
+    (check-type tag (or null symbol) "a symbol")
     `(progn
-       ,@(when label
-	   `((adppvt:push-header-tag ',label ,str)))
-       (adppvt:emplace-adp-element :header ,str ',label)
+       ,@(when tag
+	   `((adppvt:push-header-tag ',tag ,str)))
+       (adppvt:emplace-adp-element :header ,str ',tag)
        (values))))
 
 
-(adv-defmacro subheader (str &optional label)
+(adv-defmacro subheader (str &optional tag)
+  "Same as header, but add a subheader."
   (when adppvt:*add-documentation*
     (check-type str string "a string")
-    (check-type label (or null symbol) "a symbol")
+    (check-type tag (or null symbol) "a symbol")
     `(progn
-       ,@(when label
-	   `((adppvt:push-header-tag ',label ,str)))
-       (adppvt:emplace-adp-element :subheader ,str ',label)
+       ,@(when tag
+	   `((adppvt:push-header-tag ',tag ,str)))
+       (adppvt:emplace-adp-element :subheader ,str ',tag)
        (values))))
 
 
-(adv-defmacro subsubheader (str &optional label)
+(adv-defmacro subsubheader (str &optional tag)
+  "Same as header, but add a subsubheader."
   (when adppvt:*add-documentation*
     (check-type str string "a string")
-    (check-type label (or null symbol) "a symbol")
+    (check-type tag (or null symbol) "a symbol")
     `(progn
-       ,@(when label
-	   `((adppvt:push-header-tag ',label ,str)))
-       (adppvt:emplace-adp-element :subsubheader ,str ',label)
+       ,@(when tag
+	   `((adppvt:push-header-tag ',tag ,str)))
+       (adppvt:emplace-adp-element :subsubheader ,str ',tag)
        (values))))
 
 
 (adv-defmacro text (&rest objects)
+  "Add plain text. The arguments in objects can be any lisp object. They will be princ-ed and concatenated into a single string.
+You can use the following macros to enrich your text: bold, italic, bold-italic, code-inline, web-link, header-ref, symbol-ref, 
+function-ref, type-ref and file-ref."
   (when adppvt:*add-documentation*
     `(progn
        (adppvt:emplace-adp-element :text ,@objects)
@@ -117,6 +123,8 @@
 
 
 (adv-defmacro table (&rest rows)
+  "Add a table. Each argument must be a list of lists. Each inner list must have as first element the keyword :cell and the rest 
+are treated as if using the macro text."
   (when adppvt:*add-documentation*
     (loop for row in rows
 	  do (check-type row list "a list")
@@ -130,6 +138,10 @@
 
 
 (adv-defmacro itemize (&rest items)
+  "Add a list of items. Each argument must be a list. Each list must have as first argument the keyword :item or :itemize. If 
+:item is used, the rest of the elements in that list will be treated as if using the macro text. If :itemize is used the rest 
+of elements must be lists where its first elements are the keywords :item or :itemize. In other words, when :itemize is used 
+a nested list is added."
   (when adppvt:*add-documentation*
     (labels ((check-items (item-list)
 	       (loop for item in item-list
@@ -150,6 +162,8 @@
 
 
 (adv-defmacro image (alt-text path)
+  "Add an image with alt-text as the alternative text and path must be the pathname, relative to the system's root directory, 
+where the image is located."
   (when adppvt:*add-documentation*
     (check-type alt-text string "a string")
     (check-type path pathname "a pathname")
@@ -159,26 +173,31 @@
 
 
 (adv-defmacro bold (&rest args)
+  "Add bold style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string."
   (when adppvt:*add-documentation*
     `(adppvt:create-bold-text ,@args)))
 
 
 (adv-defmacro italic (&rest args)
+  "Add italic style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string."
   (when adppvt:*add-documentation*
     `(adppvt:create-italic-text ,@args)))
 
 
 (adv-defmacro bold-italic (&rest args)
+  "Add bold and italic style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string."
   (when adppvt:*add-documentation*
     `(adppvt:create-bold-italic-text ,@args)))
 
 
 (adv-defmacro code-inline (&rest code)
+  "Add inlined style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string."
   (when adppvt:*add-documentation*
     `(adppvt:create-code-inline-text ,@code)))
 
 
 (adv-defmacro web-link (name link)
+  "Add a hyperlink. The text showed is name and link must be a valid web URL. Both arguments must be strings."
   (when adppvt:*add-documentation*
     (check-type name string "a string")
     (check-type link string "a string")
@@ -186,36 +205,52 @@
 
 
 (adv-defmacro file-ref (path)
+  "Add a reference to a documentation file when using the macros text, table or itemize. The argument is the pathname, relative 
+to the system's root directory, to the referenced file. Only pathnames used with write-in-file are valid. "
   (when adppvt:*add-documentation*
     (check-type path pathname "a pathname")
     `(adppvt:create-file-ref-text ,path)))
 
 
-(adv-defmacro header-ref (label)
+(adv-defmacro header-ref (tag)
+  "Add a reference to a header when using the macros text, table or itemize. The argument is a symbol denoting a header-tag.
+Only the symbols used with the macros header, subheader and subsubheader are valid."
   (when adppvt:*add-documentation*
-    (check-type label symbol "a symbol")
-    `(adppvt:create-header-ref-text ',label)))
+    (check-type tag symbol "a symbol")
+    `(adppvt:create-header-ref-text ',tag)))
 
 
-(adv-defmacro symbol-ref (label)
+(adv-defmacro symbol-ref (tag)
+  "Add a reference to a variable when using the macros text, table or itemize. The argument is a symbol denoting a variable
+defined with adp:deconstant, adp:define-symbol-macro, adp:defparameter or adp:defvar."
   (when adppvt:*add-documentation*
-    (check-type label symbol "a symbol")
-    `(adppvt:create-symbol-ref-text ',label)))
+    (check-type tag symbol "a symbol")
+    `(adppvt:create-symbol-ref-text ',tag)))
 
 
-(adv-defmacro function-ref (label)
+(adv-defmacro function-ref (tag)
+  "Add a reference to a function symbol when using the macros text, table or itemize. The argument is a symbol denoting a function
+defined with adp:defgeneric, adp:define-modify-macro, adp:defmacro or adp:defun."
   (when adppvt:*add-documentation*
-    (check-type label symbol "a symbol")
-    `(adppvt:create-function-ref-text ',label)))
+    (check-type tag symbol "a symbol")
+    `(adppvt:create-function-ref-text ',tag)))
 
 
-(adv-defmacro type-ref (label)
+(adv-defmacro type-ref (tag)
+  "Add a reference to a type symbol when using the macros text, table or itemize. The argument is a symbol denoting a type
+defined with adp:defclass, adp:define-condition, adp:defstruct or adp:deftype."
   (when adppvt:*add-documentation*
-    (check-type label symbol "a symbol")
-    `(adppvt:create-type-ref-text ',label)))
+    (check-type tag symbol "a symbol")
+    `(adppvt:create-type-ref-text ',tag)))
 
 
 (adv-defmacro code-tag (tags &body code)
+  "Assign several tags to a piece of code. The code is placed into a progn form. The argument tags must be a list
+of symbols. If no tags are provided, then code-tag will do nothing. Each symbol in tags will be a code-tag assigned to code. 
+Inside the code-tag form it is correct to use a (code-hide tags &rest forms) form. It only indicates to code-tag 
+which parts of code can be hidden when using the tag in the macro code-block. code-hide accepts also a list of tags. If a tag 
+used in code-tag also appears in code-hide, that piece of code will be hidden when using the macro code-block. If the list of 
+tags in code-hide is empty, the that piece of code will be hidden for every tag used in code-tag."
   (with-gensyms (tag)
     `(progn
        ,@(when adppvt:*add-documentation*
@@ -228,6 +263,8 @@
 
 
 (adv-defmacro code-block (tags &body code)
+  "Add a block of code. Each element of code will be prin1-ed but not evaluated. If a symbol is used and that symbol appears as a tag in tags, then 
+the code assigned to that tag is prin1-ed instead of the symbol."
   (when adppvt:*add-documentation*
     (check-type tags list "a list")
     (loop for tag in tags
@@ -244,6 +281,7 @@
 
 
 (adv-defmacro code-example (&body code)
+  "Same as code-block, but tags cannot be used and the code is evaluated. The standard output and the results of each piece of code are also printed."
   (when adppvt:*add-documentation*
     (let ((evaluated-code (loop for expr in code
 				collect (with-gensyms (output result)
@@ -263,6 +301,7 @@
 (adv-subheader "API documentation functions" api-subheader)
 
 (adv-defmacro defclass (&body defclass-body)
+  "Add a defclass declaration. The macro expands to cl:defclass. Also, the class name is used to create a type-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-type-tag (car ',defclass-body))
@@ -271,6 +310,7 @@
 
 
 (adv-defmacro defconstant (&body defconstant-body)
+  "Add a defconstant declaration. The macro expands to cl:defconstant. Also, the constant name is used to create a symbol-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-symbol-tag (car ',defconstant-body))
@@ -279,6 +319,7 @@
 
 
 (adv-defmacro defgeneric (&body defgeneric-body)
+  "Add a defgeneric declaration. The macro expands to cl:defgeneric. Also, the generic function name is used to create a function-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-function-tag (car ',defgeneric-body))
@@ -287,6 +328,7 @@
 
 
 (adv-defmacro define-compiler-macro (&body define-compiler-macro-body)
+  "Add a define-compiler-macro declaration. The macro expands to cl:define-compiler-macro."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:emplace-adp-element :define-compiler-macro '(cl:define-compiler-macro ,@define-compiler-macro-body))))
@@ -294,6 +336,7 @@
 
 
 (adv-defmacro define-condition (&body define-condition-body)
+  "Add a define-condition declaration. The macro expands to cl:define-condition. Also, the condition name is used to create a type-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-type-tag (car ',define-condition-body))
@@ -302,6 +345,7 @@
 
 
 (adv-defmacro define-method-combination (&body define-method-combination-body)
+  "Add a define-method-combination declaration. The macro expands to cl:define-method-combination."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:emplace-adp-element :define-method-combination '(cl:define-method-combination ,@define-method-combination-body))))
@@ -309,6 +353,7 @@
 
 
 (adv-defmacro define-modify-macro (&body define-modify-macro-body)
+  "Add a define-modify-macro declaration. The macro expands to cl:define-modify-macro. Also, the macro name is used to create a function-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-function-tag (car ',define-modify-macro-body))
@@ -317,6 +362,7 @@
 
 
 (adv-defmacro define-setf-expander (&body define-setf-expander-body)
+  "Add a define-setf-expander declaration. The macro expands to cl:define-setf-expander."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:emplace-adp-element :define-setf-expander '(cl:define-setf-expander ,@define-setf-expander-body))))
@@ -324,6 +370,7 @@
 
 
 (adv-defmacro define-symbol-macro (&body define-symbol-macro-body)
+  "Add a define-symbol-macro declaration. The macro expands to cl:define-symbol-macro. Also, the symbol name is used to create a symbol-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-symbol-tag (car ',define-symbol-macro-body))
@@ -332,6 +379,7 @@
 
 
 (adv-defmacro defmacro (&body defmacro-body)
+  "Add a defmacro declaration. The macro expands to cl:defmacro. Also, the macro name is used to create a function-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-function-tag (car ',defmacro-body))
@@ -340,6 +388,7 @@
 
 
 (adv-defmacro defmethod (&body defmethod-body)
+  "Add a defmethod declaration. The macro expands to cl:defmethod."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:emplace-adp-element :defmethod '(cl:defmethod ,@defmethod-body))))
@@ -347,6 +396,7 @@
 
 
 (adv-defmacro defpackage (&body defpackage-body)
+  "Add a defpackage declaration. The macro expands to cl:defpackage."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:emplace-adp-element :defpackage '(cl:defpackage ,@defpackage-body))))
@@ -354,6 +404,7 @@
 
 
 (adv-defmacro defparameter (&body defparameter-body)
+  "Add a defparameter declaration. The macro expands to cl:defparameter. Also, the parameter name is used to create a symbol-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-symbol-tag (car ',defparameter-body))
@@ -362,6 +413,7 @@
 
 
 (adv-defmacro defsetf (&body defsetf-body)
+  "Add a defsetf declaration. The macro expands to cl:defsetf."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:emplace-adp-element :defsetf '(cl:defsetf ,@defsetf-body))))
@@ -369,6 +421,7 @@
 
 
 (adv-defmacro defstruct (&body defstruct-body)
+  "Add a defstruct declaration. The macro expands to cl:defstruct. Also, the struct name is used to create a type-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-type-tag (car ',defstruct-body))
@@ -377,6 +430,7 @@
 
 
 (adv-defmacro deftype (&body deftype-body)
+  "Add a deftype declaration. The macro expands to cl:deftype. Also, the type name is used to create a type-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-type-tag (car ',deftype-body))
@@ -385,6 +439,7 @@
 
 
 (adv-defmacro defun (&body defun-body)
+  "Add a defun declaration. The macro expands to cl:defun. Also, the function name is used to create a function-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((when (symbolp (car ',defun-body))
@@ -396,6 +451,7 @@
 
 
 (adv-defmacro defvar (&body defvar-body)
+  "Add a defvar declaration. The macro expands to cl:defvar. Also, the variable name is used to create a symbol-tag."
   `(progn
      ,@(when adppvt:*add-documentation*
 	 `((adppvt:push-symbol-tag (car ',defvar-body))
@@ -409,6 +465,10 @@
 
 
 (adv-defmacro write-in-file (file-path)
+  "Associate all the information gathered so far with the pathname file-path. This will cause the creation of a file 
+where all the information will be printed in. The pathname will be considered relative to the system's root directory.
+Only the directory and name parts of file-path are considered. The rest are ignored (including the extension). This 
+macro can be used multiple times."
   (when adppvt:*add-documentation*
     (check-type file-path pathname "a pathname")
     (assert (pathname-name file-path) (file-path) "The ~s pathname has not a name part." file-path)
@@ -438,6 +498,9 @@
 
 (declaim (ftype (function (t symbol &rest t) t) load-documentation-system))
 (adv-defun load-documentation-system (system style &rest style-args)
+  "Load a system with documentation generation activated. The style must be a keyword denoting a valid style.
+Each style will create different files. The style-args are style-dependent. In other words, each style can have its own 
+arguments to let the user customize briefly how documentation is printed."
   (assert (asdf:find-system system) (system) "The system ~s was not found." system)
   (adppvt:remove-current-procs)
   (let ((style-system (intern (concatenate 'string "ADP/" (symbol-name style)) :keyword)))
@@ -462,6 +525,9 @@
 (adv-subheader "Additional functions")
 
 (adv-defmacro cl-ref (sym)
-  `(web-link ,(prin1-to-string sym) ,(hyperspec:lookup sym)))
+  "Add a reference to a Common Lisp symbol when using the macros text, table or itemize."
+  (let ((result (hyperspec:lookup sym)))
+    (assert result () "The symbol ~s is not a valid Common Lisp symbol." sym)
+    `(web-link ,(prin1-to-string sym) ,result)))
 
 (adv-write-in-file #P"docs/user-api")
--- docs/user-api.md
+++ docs/user-api.md
@@ -5,23 +5,29 @@
 #### Macro: HEADER
 
 ```Lisp
-(defmacro ADP:HEADER (STR &OPTIONAL LABEL)
+(defmacro ADP:HEADER (STR &OPTIONAL TAG)
   ...)
 ```
+
+Add a header with name str. Also, if tag is not nil but a symbol, a new header-tag is created.
 
 #### Macro: SUBHEADER
 
 ```Lisp
-(defmacro ADP:SUBHEADER (STR &OPTIONAL LABEL)
+(defmacro ADP:SUBHEADER (STR &OPTIONAL TAG)
   ...)
 ```
+
+Same as header, but add a subheader.
 
 #### Macro: SUBSUBHEADER
 
 ```Lisp
-(defmacro ADP:SUBSUBHEADER (STR &OPTIONAL LABEL)
+(defmacro ADP:SUBSUBHEADER (STR &OPTIONAL TAG)
   ...)
 ```
+
+Same as header, but add a subsubheader.
 
 #### Macro: TEXT
 
@@ -30,12 +36,19 @@
   ...)
 ```
 
+Add plain text. The arguments in objects can be any lisp object. They will be princ-ed and concatenated into a single string.
+You can use the following macros to enrich your text: bold, italic, bold-italic, code-inline, web-link, header-ref, symbol-ref, 
+function-ref, type-ref and file-ref.
+
 #### Macro: TABLE
 
 ```Lisp
 (defmacro ADP:TABLE (&REST ROWS)
   ...)
 ```
+
+Add a table. Each argument must be a list of lists. Each inner list must have as first element the keyword :cell and the rest 
+are treated as if using the macro text.
 
 #### Macro: ITEMIZE
 
@@ -44,12 +57,20 @@
   ...)
 ```
 
+Add a list of items. Each argument must be a list. Each list must have as first argument the keyword :item or :itemize. If 
+:item is used, the rest of the elements in that list will be treated as if using the macro text. If :itemize is used the rest 
+of elements must be lists where its first elements are the keywords :item or :itemize. In other words, when :itemize is used 
+a nested list is added.
+
 #### Macro: IMAGE
 
 ```Lisp
 (defmacro ADP:IMAGE (ALT-TEXT PATH)
   ...)
 ```
+
+Add an image with alt-text as the alternative text and path must be the pathname, relative to the system's root directory, 
+where the image is located.
 
 #### Macro: BOLD
 
@@ -58,12 +79,16 @@
   ...)
 ```
 
+Add bold style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string.
+
 #### Macro: ITALIC
 
 ```Lisp
 (defmacro ADP:ITALIC (&REST ARGS)
   ...)
 ```
+
+Add italic style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string.
 
 #### Macro: BOLD-ITALIC
 
@@ -72,12 +97,16 @@
   ...)
 ```
 
+Add bold and italic style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string.
+
 #### Macro: CODE-INLINE
 
 ```Lisp
 (defmacro ADP:CODE-INLINE (&REST CODE)
   ...)
 ```
+
+Add inlined style to text when using the macros text, table or itemize. Each argument is princ-ed and concatenated into a string.
 
 #### Macro: WEB-LINK
 
@@ -86,6 +115,8 @@
   ...)
 ```
 
+Add a hyperlink. The text showed is name and link must be a valid web URL. Both arguments must be strings.
+
 #### Macro: FILE-REF
 
 ```Lisp
@@ -93,33 +124,48 @@
   ...)
 ```
 
+Add a reference to a documentation file when using the macros text, table or itemize. The argument is the pathname, relative 
+to the system's root directory, to the referenced file. Only pathnames used with write-in-file are valid. 
+
 #### Macro: HEADER-REF
 
 ```Lisp
-(defmacro ADP:HEADER-REF (LABEL)
+(defmacro ADP:HEADER-REF (TAG)
   ...)
 ```
+
+Add a reference to a header when using the macros text, table or itemize. The argument is a symbol denoting a header-tag.
+Only the symbols used with the macros header, subheader and subsubheader are valid.
 
 #### Macro: SYMBOL-REF
 
 ```Lisp
-(defmacro ADP:SYMBOL-REF (LABEL)
+(defmacro ADP:SYMBOL-REF (TAG)
   ...)
 ```
+
+Add a reference to a variable when using the macros text, table or itemize. The argument is a symbol denoting a variable
+defined with adp:deconstant, adp:define-symbol-macro, adp:defparameter or adp:defvar.
 
 #### Macro: FUNCTION-REF
 
 ```Lisp
-(defmacro ADP:FUNCTION-REF (LABEL)
+(defmacro ADP:FUNCTION-REF (TAG)
   ...)
 ```
+
+Add a reference to a function symbol when using the macros text, table or itemize. The argument is a symbol denoting a function
+defined with adp:defgeneric, adp:define-modify-macro, adp:defmacro or adp:defun.
 
 #### Macro: TYPE-REF
 
 ```Lisp
-(defmacro ADP:TYPE-REF (LABEL)
+(defmacro ADP:TYPE-REF (TAG)
   ...)
 ```
+
+Add a reference to a type symbol when using the macros text, table or itemize. The argument is a symbol denoting a type
+defined with adp:defclass, adp:define-condition, adp:defstruct or adp:deftype.
 
 #### Macro: CODE-TAG
 
@@ -128,6 +174,13 @@
   ...)
 ```
 
+Assign several tags to a piece of code. The code is placed into a progn form. The argument tags must be a list
+of symbols. If no tags are provided, then code-tag will do nothing. Each symbol in tags will be a code-tag assigned to code. 
+Inside the code-tag form it is correct to use a (code-hide tags &rest forms) form. It only indicates to code-tag 
+which parts of code can be hidden when using the tag in the macro code-block. code-hide accepts also a list of tags. If a tag 
+used in code-tag also appears in code-hide, that piece of code will be hidden when using the macro code-block. If the list of 
+tags in code-hide is empty, the that piece of code will be hidden for every tag used in code-tag.
+
 #### Macro: CODE-BLOCK
 
 ```Lisp
@@ -135,12 +188,17 @@
   ...)
 ```
 
+Add a block of code. Each element of code will be prin1-ed but not evaluated. If a symbol is used and that symbol appears as a tag in tags, then 
+the code assigned to that tag is prin1-ed instead of the symbol.
+
 #### Macro: CODE-EXAMPLE
 
 ```Lisp
 (defmacro ADP:CODE-EXAMPLE (&BODY CODE)
   ...)
 ```
+
+Same as code-block, but tags cannot be used and the code is evaluated. The standard output and the results of each piece of code are also printed.
 
 ## API documentation functions
 
@@ -151,12 +209,16 @@
   ...)
 ```
 
+Add a defclass declaration. The macro expands to cl:defclass. Also, the class name is used to create a type-tag.
+
 #### Macro: DEFCONSTANT
 
 ```Lisp
 (defmacro ADP:DEFCONSTANT (&BODY DEFCONSTANT-BODY)
   ...)
 ```
+
+Add a defconstant declaration. The macro expands to cl:defconstant. Also, the constant name is used to create a symbol-tag.
 
 #### Macro: DEFGENERIC
 
@@ -165,12 +227,16 @@
   ...)
 ```
 
+Add a defgeneric declaration. The macro expands to cl:defgeneric. Also, the generic function name is used to create a function-tag.
+
 #### Macro: DEFINE-COMPILER-MACRO
 
 ```Lisp
 (defmacro ADP:DEFINE-COMPILER-MACRO (&BODY DEFINE-COMPILER-MACRO-BODY)
   ...)
 ```
+
+Add a define-compiler-macro declaration. The macro expands to cl:define-compiler-macro.
 
 #### Macro: DEFINE-CONDITION
 
@@ -179,12 +245,16 @@
   ...)
 ```
 
+Add a define-condition declaration. The macro expands to cl:define-condition. Also, the condition name is used to create a type-tag.
+
 #### Macro: DEFINE-METHOD-COMBINATION
 
 ```Lisp
 (defmacro ADP:DEFINE-METHOD-COMBINATION (&BODY DEFINE-METHOD-COMBINATION-BODY)
   ...)
 ```
+
+Add a define-method-combination declaration. The macro expands to cl:define-method-combination.
 
 #### Macro: DEFINE-MODIFY-MACRO
 
@@ -193,12 +263,16 @@
   ...)
 ```
 
+Add a define-modify-macro declaration. The macro expands to cl:define-modify-macro. Also, the macro name is used to create a function-tag.
+
 #### Macro: DEFINE-SETF-EXPANDER
 
 ```Lisp
 (defmacro ADP:DEFINE-SETF-EXPANDER (&BODY DEFINE-SETF-EXPANDER-BODY)
   ...)
 ```
+
+Add a define-setf-expander declaration. The macro expands to cl:define-setf-expander.
 
 #### Macro: DEFINE-SYMBOL-MACRO
 
@@ -207,12 +281,16 @@
   ...)
 ```
 
+Add a define-symbol-macro declaration. The macro expands to cl:define-symbol-macro. Also, the symbol name is used to create a symbol-tag.
+
 #### Macro: DEFMACRO
 
 ```Lisp
 (defmacro ADP:DEFMACRO (&BODY DEFMACRO-BODY)
   ...)
 ```
+
+Add a defmacro declaration. The macro expands to cl:defmacro. Also, the macro name is used to create a function-tag.
 
 #### Macro: DEFMETHOD
 
@@ -221,12 +299,16 @@
   ...)
 ```
 
+Add a defmethod declaration. The macro expands to cl:defmethod.
+
 #### Macro: DEFPACKAGE
 
 ```Lisp
 (defmacro ADP:DEFPACKAGE (&BODY DEFPACKAGE-BODY)
   ...)
 ```
+
+Add a defpackage declaration. The macro expands to cl:defpackage.
 
 #### Macro: DEFPARAMETER
 
@@ -235,12 +317,16 @@
   ...)
 ```
 
+Add a defparameter declaration. The macro expands to cl:defparameter. Also, the parameter name is used to create a symbol-tag.
+
 #### Macro: DEFSETF
 
 ```Lisp
 (defmacro ADP:DEFSETF (&BODY DEFSETF-BODY)
   ...)
 ```
+
+Add a defsetf declaration. The macro expands to cl:defsetf.
 
 #### Macro: DEFSTRUCT
 
@@ -249,12 +335,16 @@
   ...)
 ```
 
+Add a defstruct declaration. The macro expands to cl:defstruct. Also, the struct name is used to create a type-tag.
+
 #### Macro: DEFTYPE
 
 ```Lisp
 (defmacro ADP:DEFTYPE (&BODY DEFTYPE-BODY)
   ...)
 ```
+
+Add a deftype declaration. The macro expands to cl:deftype. Also, the type name is used to create a type-tag.
 
 #### Macro: DEFUN
 
@@ -263,12 +353,16 @@
   ...)
 ```
 
+Add a defun declaration. The macro expands to cl:defun. Also, the function name is used to create a function-tag.
+
 #### Macro: DEFVAR
 
 ```Lisp
 (defmacro ADP:DEFVAR (&BODY DEFVAR-BODY)
   ...)
 ```
+
+Add a defvar declaration. The macro expands to cl:defvar. Also, the variable name is used to create a symbol-tag.
 
 ## Documentation writer function
 
@@ -279,12 +373,21 @@
   ...)
 ```
 
+Associate all the information gathered so far with the pathname file-path. This will cause the creation of a file 
+where all the information will be printed in. The pathname will be considered relative to the system's root directory.
+Only the directory and name parts of file-path are considered. The rest are ignored (including the extension). This 
+macro can be used multiple times.
+
 #### Function: LOAD-DOCUMENTATION-SYSTEM
 
 ```Lisp
 (defun ADP:LOAD-DOCUMENTATION-SYSTEM (SYSTEM STYLE &REST STYLE-ARGS)
   ...)
 ```
+
+Load a system with documentation generation activated. The style must be a keyword denoting a valid style.
+Each style will create different files. The style-args are style-dependent. In other words, each style can have its own 
+arguments to let the user customize briefly how documentation is printed.
 
 ## Additional functions
 
@@ -294,4 +397,6 @@
 (defmacro CL-REF (SYM)
   ...)
 ```
+
+Add a reference to a Common Lisp symbol when using the macros text, table or itemize.
 
